fa75c07209891f0082a30f472e11dc973f216720
Gulistane Raza <gulistaneraza01@gmail.com>
2026-03-24 22:32:08 +0530

    Enhance hello.go by adding random number generation and mathematical calculations, including square root and PI value, while updating the greeting message.

--- getting-started/hello.go
+++ getting-started/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 
 	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Hello, World!", "Raza")
     fmt.Println(quote.Go())
+
+    fmt.Println("random number=", rand.Float32())
+
+	fmt.Println("sqrt root of 144 is: ",math.Sqrt(142))
+
+	fmt.Println("PI value is: ",math.Pi)
 }
