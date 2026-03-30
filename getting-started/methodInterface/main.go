02260dec993872e0ad3d82923fd6b61ab4668d3b
Gulistane Raza <gulistaneraza01@gmail.com>
2026-03-30 22:01:05 +0530

    chore(methodInterface): Comment out fibonacci function for clarity
    Temporarily comment out the fibonacci function to improve code readability
    and facilitate development. This change does not affect the existing functionality.

--- getting-started/methodInterface/main.go
+++ getting-started/methodInterface/main.go
@@ -19,9 +19,9 @@ func main() {
 	fmt.Println(result)
 }
 
-func fibo(n int) int {
-	if n <= 1 {
-		return n
-	}
-	return fibo(n-1) + fibo(n-2)
-}
+// func fibo(n int) int {
+// 	if n <= 1 {
+// 		return n
+// 	}
+// 	return fibo(n-1) + fibo(n-2)
+// }
