c76b13a53258930beb3fc3a8ce4d7e0bbdf6a7f6
Gulistane Raza <gulistaneraza01@gmail.com>
2026-04-03 23:04:59 +0530

    chore(methodinterface): refactor vertex struct and add scaling functions
    Introduce a Vertex struct with methods for scaling.
    The main function is updated to demonstrate the new functionality,
    while previous examples are commented out for clarity.
    This enhances the code structure and prepares for further development.

--- getting-started/methodInterface/main.go
+++ getting-started/methodInterface/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Number struct {
 	Num1 int
 	Num2 int
@@ -11,17 +9,40 @@ func (n Number) add() int {
 	return n.Num1 + n.Num2
 }
 
-func main() {
-	addTwoNumber := Number{2, 3}
-
-	result := addTwoNumber.add()
-
-	fmt.Println(result)
+type Vertex struct {
+	X float64
+	Y float64
 }
 
-// func fibo(n int) int {
-// 	if n <= 1 {
-// 		return n
-// 	}
-// 	return fibo(n-1) + fibo(n-2)
-// }
+func test(v *Vertex) {
+	v.X = 100
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	// addTwoNumber := Number{2, 3}
+
+	// result := addTwoNumber.add()
+
+	// fmt.Println(result)
+
+	// v := Vertex{1, 2}
+
+	// test(&v)
+
+	// fmt.Println(v)
+
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+}
