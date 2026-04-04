879265a0878df903cee4457d4b5e8aa17684a7fb
Gulistane Raza <gulistaneraza01@gmail.com>
2026-04-04 22:54:35 +0530

    chore(methodinterface): add animal interface and dog struct with sound
    Introduce an Animal interface and a Dog struct that implements the Sound method.
    The main function is updated to demonstrate type assertion and interface usage,
    enhancing the code's extensibility and showcasing polymorphism.

--- getting-started/methodInterface/main.go
+++ getting-started/methodInterface/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Number struct {
 	Num1 int
 	Num2 int
@@ -28,6 +30,19 @@ func ScaleFunc(v *Vertex, f float64) {
 	v.Y = v.Y * f
 }
 
+type Animal interface {
+	Sound() string
+}
+
+type Dog struct {
+	Name string
+	Age  int
+}
+
+func (d Dog) Sound() string {
+	return "Woof"
+}
+
 func main() {
 	// addTwoNumber := Number{2, 3}
 
@@ -41,8 +56,36 @@ func main() {
 
 	// fmt.Println(v)
 
-	v := Vertex{3, 4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
+	// v := Vertex{3, 4}
+	// v.Scale(2)
+	// ScaleFunc(&v, 10)
 
+	// animal := Dog{"Buddy", 24}
+	// fmt.Println(animal.Age, animal.Name)
+
+	// var i interface{} = 1
+
+	// value, err := i.(int)
+	// fmt.Println(value, err)
+
+	var i interface{} = 4
+	do(i)
+
+}
+
+func random(i interface{}) {
+	fmt.Println(i)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("a", i, v)
+
+	case string:
+		fmt.Println("hello")
+
+	default:
+		fmt.Println("invalid")
+	}
 }
